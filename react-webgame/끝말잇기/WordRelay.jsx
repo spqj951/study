a515eb181e0710ccedf23dcdda57986865ccbd1f
kyujin <spqj951@gmail.com>
2022-08-08 23:26:11 +0900

    약간 건들여보기

---
+++ react-webgame/끝말잇기/WordRelay.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+function WordRelay() {}
+
+export default WordRelay;
--- react-webgame/끝말잇기/client.jsx
+++ react-webgame/끝말잇기/client.jsx
@@ -1,2 +1,4 @@
 import React from "react";
 import ReactDom from "react-dom";
+
+import WordRelay from "./WordRelay";
